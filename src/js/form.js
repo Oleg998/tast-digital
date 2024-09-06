11f65e847b4af90a0659cc93288b7a016671d61c
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-09-06 15:03:55 +0300

    1

--- src/js/form.js
+++ src/js/form.js
@@ -1,41 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const input = document.querySelector("#phone");
-  
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.querySelector('#phone');
+
   const iti = window.intlTelInput(input, {
-    initialCountry: "auto",
-    geoIpLookup: function(callback) {
+    initialCountry: 'auto',
+    geoIpLookup: function (callback) {
       fetch('https://ipinfo.io?token=d37d4a3994d8a5')
-        .then(function(response) { return response.json(); })
-        .then(function(data) { callback(data.country); })
-        .catch(function() { callback('US'); });
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          callback(data.country);
+        })
+        .catch(function () {
+          callback('US');
+        });
     },
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
+    utilsScript:
+      'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js',
   });
 
-  document.getElementById("callback").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    if (iti.isValidNumber()) {
-      window.location.href = "./thankYou.html";
-    } else {
-      console.error("Invalid phone number");
-    }
-  });
-});;
+  document
+    .getElementById('callback')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
 
-
+      if (iti.isValidNumber()) {
+        window.location.href = './thankYou.html';
+      } else {
+        console.error('Invalid phone number');
+      }
+    });
+});
 
 const callback = document.getElementById('callback');
 const elements = document.querySelectorAll('a, button');
 
 elements.forEach(element => {
-    element.addEventListener('click', function(e) {
-        
-        if (!element.classList.contains('menu-btn-back') && !element.classList.contains('form-btn')) {
-            e.preventDefault();
-            callback.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    });
+  element.addEventListener('click', function (e) {
+    if (
+      !element.classList.contains('menu-btn-back') &&
+      !element.classList.contains('form-btn')
+    ) {
+      e.preventDefault();
+      callback.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
 });
